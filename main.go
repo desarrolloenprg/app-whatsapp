5c87f7d9a2e3a17e32625a0d2e296661798b6b3f
kenshin <kenshin.developer@gmail.com>
2018-10-09 06:58:06 -0400

    creacion del template base

--- main.go
+++ main.go
@@ -1,112 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"time"
-
-	whatsapp "github.com/Rhymen/go-whatsapp"
+	"html/template"
+	"net/http"
 )
 
-type myHandler struct{}
-
-func (myHandler) HandleError(err error) {
-	fmt.Fprintf(os.Stderr, "%v", err)
-}
-
-func (myHandler) HandleTextMessage(message whatsapp.TextMessage) {
-	fmt.Println(message)
-}
-
-func (myHandler) HandleImageMessage(message whatsapp.ImageMessage) {
-	fmt.Println(message)
-}
-
-func (myHandler) HandleVideoMessage(message whatsapp.VideoMessage) {
-	fmt.Println(message)
-}
-
-func (myHandler) HandleJsonMessage(message string) {
-	fmt.Println(message)
-}
-
-func saveSession(session whatsapp.Session) {
-	file, err := os.Create("info_session.txt")
-	if err != nil {
-		log.Fatalf("Error al crear el fichero de almacenamiento de la sesion.\n")
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	writer.WriteString(session.ClientId + "\n")
-	writer.Flush()
-	writer.WriteString(session.ClientToken + "\n")
-	writer.WriteString(string(session.EncKey) + "\n")
-	writer.WriteString(string(session.MacKey) + "\n")
-	writer.WriteString(session.ServerToken + "\n")
-	writer.WriteString(session.Wid + "\n")
-}
-
-func sendMessage(wac *whatsapp.Conn, number string, msg string) {
-	text := whatsapp.TextMessage{
-		Info: whatsapp.MessageInfo{
-			RemoteJid: number + "@s.whatsapp.net",
-		},
-		Text: msg,
-	}
-	err := wac.Send(text)
-	if err != nil {
-		fmt.Printf("error al enviar el msg a '%s'.\n", number)
-	} else {
-		fmt.Printf("mensaje enviado a '%s'.\n", number)
-	}
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./src/templates/base.html", "./src/templates/home.html"))
+	t.ExecuteTemplate(w, "base", nil)
 }
 
 func main() {
-	wac, err := whatsapp.NewConn(30 * time.Second)
-	if err != nil {
-		log.Fatalf("Error al establecer la conexion...")
-	}
+	cssFolder := http.FileServer(http.Dir("src/css"))
+	http.Handle("/css/", http.StripPrefix("/css/", cssFolder))
 
-	qrChan := make(chan string)
-	go func() {
-		fmt.Printf("qr code: %v\n", <-qrChan)
-		//show qr code or save it somewhere to scan
-	}()
-	sess, err := wac.Login(qrChan)
-	if err != nil {
-		log.Fatalf("Error al autentificarse...")
-	}
-	saveSession(sess)
+	imgFolder := http.FileServer(http.Dir("src/img"))
+	http.Handle("/img/", http.StripPrefix("/img/", imgFolder))
 
-	// ca, err := wac.RestoreSession(sess)
-	fmt.Printf("id: %d\n", sess.ClientId)
-	// wac.AddHandler(myHandler{})
+	jsFolder := http.FileServer(http.Dir("src/js"))
+	http.Handle("/js/", http.StripPrefix("/js/", jsFolder))
 
-	for i := 0; i < 10; i++ {
-		sendMessage(wac, "584122106942", "ULTIMA (por hoy, de pana gracias XD), prueba de software para envio masivo por whatsapp") //mio
-		sendMessage(wac, "584241951497", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp")  //benjamin
-		sendMessage(wac, "593961165103", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp")  //valeria
-		sendMessage(wac, "56932741963", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp")   //rosario
+	scssFolder := http.FileServer(http.Dir("src/scss"))
+	http.Handle("/scss/", http.StripPrefix("/scss/", scssFolder))
 
-		sendMessage(wac, "34649948216", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp")  //javier
-		sendMessage(wac, "593979816906", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp") //german
-		sendMessage(wac, "770741394", "ULTIMA (por hoy, de pana gracias XD),prueba de software para envio masivo por whatsapp")    //armando
-	}
+	vendorFolder := http.FileServer(http.Dir("src/vendor"))
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", vendorFolder))
 
-	fmt.Printf("\nTodos los mensajes han sidos enviados.\n")
-
-	// text := whatsapp.TextMessage{
-	// 	Info: whatsapp.MessageInfo{
-	// 		RemoteJid: "584122106942@s.whatsapp.net",
-	// 	},
-	// 	Text: "Hello Whatsapp",
-	// }
-
-	// err = wac.Send(text)
-	// if err != nil {
-	// 	log.Fatalf("error al enviar el msg...")
-	// }
+	fmt.Printf("Escuchando...")
+	http.HandleFunc("/", handleHomePage)
+	http.ListenAndServe(":8080", nil)
 }
