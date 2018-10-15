af99643881e0a01a051afd68a84a3691d8e01ec9
kenshin <kenshin.developer@gmail.com>
2018-10-15 19:10:59 -0400

    desarrollo de la interfaz y el envio individual

--- main.go
+++ main.go
@@ -12,10 +12,82 @@ import (
 )
 
 type Data struct {
-	Text string
+	Text  string
+	Error string
 }
 
 var qrString = ""
+var flagConn = false
+var connErr = 0
+var wacConn *whatsapp.Conn
+
+func handleIndexPage(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/home", http.StatusFound)
+}
+
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./src/templates/base.html", "./src/templates/home.html"))
+
+	data := Data{}
+	switch connErr {
+	case 1:
+		data.Error = "Error, no se ha establecido la conexión con whatsapp."
+	case 2:
+		data.Error = "Error, no es posible enviar msg con el tipo actual."
+	case 10:
+		data.Error = "Mensaje enviado."
+	case 11:
+		data.Error = "Mensaje NO enviado."
+	}
+
+	t.ExecuteTemplate(w, "base", data)
+}
+
+func handleQrCodePage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./src/templates/base.html", "./src/templates/qrcode.html"))
+	data := Data{Text: qrString}
+	t.ExecuteTemplate(w, "base", data)
+}
+
+func handleSendData(w http.ResponseWriter, r *http.Request) {
+	dataType := r.FormValue("send-type")
+	dataPhone := r.FormValue("send-phone")
+	dataMsg := r.FormValue("send-msg")
+
+	fmt.Printf("type: %s\n", dataType)
+	fmt.Printf("phone: %s\n", dataPhone)
+	fmt.Printf("msg: %s\n", dataMsg)
+
+	if flagConn && dataType == "0" {
+		sendMessage(wacConn, dataPhone, dataMsg)
+	} else if !flagConn {
+		connErr = 1
+	} else if dataType != "0" {
+		connErr = 2
+	}
+
+	http.Redirect(w, r, "/home", http.StatusFound)
+}
+
+func main() {
+	imgFolder := http.FileServer(http.Dir("src/img"))
+	http.Handle("/img/", http.StripPrefix("/img/", imgFolder))
+
+	vendorFolder := http.FileServer(http.Dir("src/vendor"))
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", vendorFolder))
+
+	fmt.Printf("Escuchando...\n")
+	http.HandleFunc("/", handleIndexPage)
+	http.HandleFunc("/home", handleHomePage)
+	http.HandleFunc("/sendata", handleSendData)
+	http.HandleFunc("/qrcode", handleQrCodePage)
+	go connWhatsapp()
+	http.ListenAndServe(":8080", nil)
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+//--------------------------------------------------
 
 func sendMessage(wac *whatsapp.Conn, number string, msg string) {
 	text := whatsapp.TextMessage{
@@ -26,39 +98,13 @@ func sendMessage(wac *whatsapp.Conn, number string, msg string) {
 	}
 	err := wac.Send(text)
 	if err != nil {
+		connErr = 11
 		fmt.Printf("error al enviar el msg a '%s'.\n", number)
 	} else {
+		connErr = 10
 		fmt.Printf("mensaje enviado a '%s'.\n", number)
 	}
 }
-
-// http.Redirect(w, r, "/home/", http.StatusFound)
-func handleHomePage(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("./src/templates/base.html", "./src/templates/home.html"))
-	t.ExecuteTemplate(w, "base", nil)
-}
-
-func handleQrCodePage(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("./src/templates/base.html", "./src/templates/qrcode.html"))
-	data := Data{Text: qrString}
-	t.ExecuteTemplate(w, "base", data)
-}
-
-func main() {
-	vendorFolder := http.FileServer(http.Dir("src/vendor"))
-	http.Handle("/vendor/", http.StripPrefix("/vendor/", vendorFolder))
-
-	fmt.Printf("Escuchando...")
-	// http.HandleFunc("/", handleHomePage)
-	http.HandleFunc("/home", handleHomePage)
-	http.HandleFunc("/qrcode", handleQrCodePage)
-	// go connWhatsapp()
-	http.ListenAndServe(":8080", nil)
-}
-
-//--------------------------------------------------
-//--------------------------------------------------
-//--------------------------------------------------
 
 func connWhatsapp() {
 	wac, err := whatsapp.NewConn(60 * time.Second)
@@ -67,10 +113,9 @@ func connWhatsapp() {
 	}
 	qrChan := make(chan string)
 	go func() {
-		// fmt.Printf("qr code: %v\n", <-qrChan)
 		qrString = <-qrChan
-		fmt.Printf("qrString: %s\n\n", qrString)
-		// err := exec.Command("rundll32", "url.dll,FileProtocolHandler", "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrString).Start()
+		// fmt.Printf("qrString: %s\n\n", qrString)
+		fmt.Printf("abriendo pestaña...\n")
 		err := exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://127.0.0.1:8080/qrcode").Start()
 		if err != nil {
 			log.Fatalf("Error al abrir el navegador.\n")
@@ -81,14 +126,15 @@ func connWhatsapp() {
 	if err != nil {
 		log.Fatalf("Error al autentificarse...")
 	}
+	fmt.Printf("abriendo pestaña...\n")
 	err = exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://127.0.0.1:8080/home").Start()
 	if err != nil {
 		log.Fatalf("Error al abrir el navegador.\n")
 	}
-	fmt.Printf("id: %d\n", sess.ClientId)
-
-	sendMessage(wac, "584122106942", "prueba de software para envio masivo por whatsapp") //mio
-	sendMessage(wac, "584241951497", "prueba de software para envio masivo por whatsapp") //benjamin
-	fmt.Printf("\nTodos los mensajes han sidos enviados.\n")
-	fmt.Printf("Entro!")
+	fmt.Printf("id: %s\n", sess.ClientId)
+	flagConn = true
+	wacConn = wac
+	// sendMessage(wac, "584122106942", "prueba de software para envio masivo por whatsapp") //mio
+	// sendMessage(wac, "584241951497", "prueba de software para envio masivo por whatsapp") //benjamin
+	// fmt.Printf("\nTodos los mensajes han sidos enviados.\n")
 }
